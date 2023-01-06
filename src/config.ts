a557e97e5415a2b6b0c155dc4568ed96c672c8c8
Douglas <mendes-barreto@Live.com>
2023-01-05 20:46:08 -0500

    chore: Add config file to project

---
+++ src/config.ts
@@ -0,0 +1,23 @@
+const nodeEnv = process.env.NODE_ENV ?? 'development';
+const isDeveloment = !process.env.NODE_ENV || nodeEnv === 'development';
+const isProduction = nodeEnv === 'production';
+
+type ServerEnv = 'local' | 'development' | 'production';
+const serverEnv: ServerEnv =
+    (process.env.SERVER_ENV as ServerEnv) || 'development';
+
+const development = {
+    nodeEnv,
+    serverEnv,
+    isDeveloment,
+    isProduction,
+    routesPrefix: process.env.ROUTES_PREFIX ?? '/api/module-user/',
+    port: process.env.PORT ?? 4000,
+    mongodb: {
+        uri: process.env.MONGODB_URI ?? 'mongodb://0.0.0.0:4222',
+    },
+    jwtSecrete: process.env.JWT_SECRET ?? 'topSecreteKey',
+    httpTimeout: 10000,
+};
+
+export { development as config };
